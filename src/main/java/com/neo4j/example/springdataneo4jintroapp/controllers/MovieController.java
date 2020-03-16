7211b8faf232aefa0c4010cbfccf09eb5a50e7d2
Toparvion <toparvion@gmx.com>
2020-03-16 08:10:35 +0700

    Added a couple of methods to both MovieController and MovieRepository
    In order to stress the difference between regexp-like and contains-like queries

--- src/main/java/com/neo4j/example/springdataneo4jintroapp/controllers/MovieController.java
+++ src/main/java/com/neo4j/example/springdataneo4jintroapp/controllers/MovieController.java
@@ -2,10 +2,14 @@ package com.neo4j.example.springdataneo4jintroapp.controllers;
 
 import com.neo4j.example.springdataneo4jintroapp.domainClasses.Movie;
 import com.neo4j.example.springdataneo4jintroapp.repositories.MovieRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Jennifer Reif
+ * @author Vladimir Plizga
  */
 @RestController
 @RequestMapping("/movies")
@@ -29,5 +33,15 @@ public class MovieController {
     @GetMapping("/search/{title}")
     public Iterable<Movie> findMovieByTitleLike(@PathVariable String title) {
         return movieRepository.findMovieByTitleLike(title);
+    }
+
+    @GetMapping("/search/by-title/{word}")
+    public Iterable<Movie> findMovieByTitleContaining(@PathVariable String word) {
+        return movieRepository.findMovieByTitleContaining(word);
+    }
+
+    @GetMapping("/search/by-description/{word}")
+    public Iterable<Movie> findTop3ByDescriptionContaining(@PathVariable String word) {
+        return movieRepository.findTop3ByDescriptionContaining(word);
     }
 }
--- src/main/java/com/neo4j/example/springdataneo4jintroapp/repositories/MovieRepository.java
+++ src/main/java/com/neo4j/example/springdataneo4jintroapp/repositories/MovieRepository.java
@@ -5,10 +5,25 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 /**
  * @author Jennifer Reif
+ * @author Vladimir Plizga
  */
 public interface MovieRepository extends Neo4jRepository<Movie, Long> {
 
     Movie getMovieByTitle(String title);
 
+    /**
+     * Searches for movies by given regexp, e.g. {@code .*Matrix}
+     * @param title the regular expression to match
+     * @return movies matched the query
+     */
     Iterable<Movie> findMovieByTitleLike(String title);
+
+    /**
+     * Searches for movies whose title contains given word, e.g. {@code Matrix}
+     * @param word a substring to search the title for
+     * @return movies matched the query
+     */
+    Iterable<Movie> findMovieByTitleContaining(String word);
+
+    Iterable<Movie> findTop3ByDescriptionContaining(String word);
 }
